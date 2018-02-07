a9439bc2d13ca791c20c3ba940d60b58610c553e
Jayden <jy412134@dal.ca>
2018-02-07 14:27:21 -0400

    Espresso Test

--- app/src/androidTest/java/com/example/jaydentaylor/password_validator/ValidatorTest.java
+++ app/src/androidTest/java/com/example/jaydentaylor/password_validator/ValidatorTest.java
@@ -1,16 +1,42 @@
 package com.example.jaydentaylor.password_validator;
 
-import org.junit.Test;
+import static android.support.test.espresso.Espresso.*;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
-import static org.junit.Assert.*;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+
+
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 /**
  * Created by jaydentaylor on 2018-02-04.
  */
+@RunWith(AndroidJUnit4.class)
 public class ValidatorTest {
+
+    @Rule
+    public ActivityTestRule<Validator> mActivityRule =
+            new ActivityTestRule(Validator.class);
+
     @Test
-    public void validate() throws Exception {
-        assertEquals(true, true);
+    public void UITestPass() throws Exception {
+        onView(withId(R.id.editText)).perform(typeText("helloT223"));
+        onView(withId(R.id.button)).perform(click());
+        onView(withText("Secure!")).check(matches(isDisplayed()));
     }
 
+    @Test
+    public void UITestFail() throws Exception {
+        onView(withId(R.id.button)).perform(click());
+        onView(withText("Not Secure!")).check(matches(isDisplayed()));
+    }
 }
